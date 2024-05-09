77e5c4999efa4ba303da973f8b5ba7ff44407616
dev <deveshchhabra@dev.com>
2024-05-09 14:22:36 +0530

    setup the tailwind and scforld

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-violet-300 text-3xl font-bold text-green-800">
+      Nameste  Everyone,let's build Netflix Gpt
+    </div>
+  );
+}
+
+export default App;
